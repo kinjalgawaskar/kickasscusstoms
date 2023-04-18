549ac05b76b87ed3f7f160bc210a0faaa9d565f6
kinjalgawaskar <gawak052@newschool.edu>
2023-04-18 02:29:56 -0400

    created kickass new page

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,34 @@ window.stateCallback = () => {
   
   //on-click event for carousel//
   
+
+
+  window.arenaCallback = () => {
+    // Put your JavaScript in here!
+    // This will run after Are.na’s API returns your data.
+  
+  
+  const images = document.querySelectorAll('img')
+  
+  console.log(images)
+  
+  //Get all of my image blocks
+  let imageBlocks = document.querySelectorAll('li.image-blocks')
+  console.log(imageBlocks)
+  = () => {
+  imageBlocks.forEach((block)) {
+  
+    //Make the blocks react on click
+    block.onClick = () => {
+      
+      //Add a class called active
+      block.classList.add ('active')
+  }
+  
+      //Remove a class called active
+  block.querySelector('aside').onClick = () => {
+    block.classList.remove ('active')
+  }
+  }
+  }
+  
