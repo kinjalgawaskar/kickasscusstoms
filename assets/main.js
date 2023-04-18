4e2d1a3ff98e2f4988e2c0228149e8b954240438
kinjalgawaskar <gawak052@newschool.edu>
2023-04-18 16:00:30 -0400

    Reworded tags in main.js for comprehension

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,38 @@
 
-  // Get the elements that you want to make draggable and droppable
+  // Making shoe element droppable
 const donkey = document.getElementById('kickass');
 const shoe = document.getElementById('kick');
 
-// Add event listeners to the draggable elements
+// Adding event listeners to the shoe
 shoe.addEventListener('dragstart', dragStart);
 
-// Add event listeners to the droppable elements
+// Adding event listeners to the shoe
 donkey.addEventListener('dragover', dragOver);
 donkey.addEventListener('drop', drop);
 
-// Function to handle the start of the drag event
+// Function to handle the start of the dragging the shoe
 function dragStart(event) {
-  // Set the data that will be transferred during the drag
   event.dataTransfer.setData('text', event.target.id);
 }
 
-// Function to handle the dragover event
+// Function to handle the shoe dragover event
 function dragOver(event) {
-  // Prevent the default action of the browser
+  // Adding prevention of the default browser action (just for my personal reference)
   event.preventDefault();
 }
 
-// Function to handle the drop event
+// Adding function to handle the drop event
 function drop(event) {
-  // Prevent the default action of the browser
+  // Adding prevention of the default browser action (just for my personal reference)
   event.preventDefault();
 
-  // Get the ID of the dragged element from the data transfer
+  // Fetching the shoe ID from the data transfer
   const id = event.dataTransfer.getData('text');
 
-  // Get the dragged element
+  // Get the shoe
   const element = document.getElementById(id);
 
-  // Append the dragged element to the drop target
+  // Appending the shoe to the drop target
   event.target.appendChild(element);
   window.open("customs.html", "_blank");
 
