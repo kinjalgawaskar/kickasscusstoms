192eafdbb92a9cd63e7e2ba182d02dcbd16378d6
kinjalgawaskar <gawak052@newschool.edu>
2023-04-18 21:26:51 -0400

    added random shuffle event listener to images

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,40 @@
-window.arenaCallback = () => {
-	// Put your JavaScript in here!
-	// This will run after Are.na’s API returns your data.
 
-  window.onscroll = function() {myFunction()};
-
-var sticky = header.offsetTop;
-let body = querySelector('body')
-let imageButton = querySelector('.image-button')
-let linksButton = querySelector('.links-button')
-let allImages = querySelector('.#image-block')
-let allLinks = querySelector('.#link-block')
-
-
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+  // Making shoe element droppable
+  const donkey = document.getElementById('kickass');
+  const shoe = document.getElementById('kick');
+  
+  // Adding event listeners to the shoe
+  shoe.addEventListener('dragstart', dragStart);
+  
+  // Adding event listeners to the shoe
+  donkey.addEventListener('dragover', dragOver);
+  donkey.addEventListener('drop', drop);
+  
+  // Function to handle the start of the dragging the shoe
+  function dragStart(event) {
+    event.dataTransfer.setData('text', event.target.id);
   }
-}
-
-window.onscroll = function() {myFunction()};
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+  
+  // Function to handle the shoe dragover event
+  function dragOver(event) {
+    // Adding prevention of the default browser action (just for my personal reference)
+    event.preventDefault();
   }
-}
-
-imageButton.onClick = function() {
-  console.log('images')
-  //add all images
-  body.appendChild(allImages)
-  //remove all links
-  body.removeChild(allLinks)
-  };
-
-
-linksButton.onClick = function() {
-  console.log('links')
-  //remove all links
-  body.appendChild(allLinks)
-  //add all images
-  body.removeChild(allImages)
-  };
-
-
-// Get all the menu items
-const menuItems = document.querySelectorAll(".menu-item");
-
-// Add a click event listener to each menu item
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    // Remove the active class from all menu items
-    menuItems.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    // Add the active class to the clicked menu item
-    item.classList.add("active");
-  });
-});
-
-}
-
+  
+  // Adding function to handle the drop event
+  function drop(event) {
+    // Adding prevention of the default browser action (just for my personal reference)
+    event.preventDefault();
+  
+    // Fetching the shoe ID from the data transfer
+    const id = event.dataTransfer.getData('text');
+  
+    // Get the shoe
+    const element = document.getElementById(id);
+  
+    // Appending the shoe to the drop target
+    event.target.appendChild(element);
+    window.open("customs.html", "_blank");
+  
+  }
+  
