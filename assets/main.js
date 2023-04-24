0dabbd51e15d69f71dfcdc3d294234782ba8d95d
kinjalgawaskar <gawak052@newschool.edu>
2023-04-24 15:48:54 -0400

    trying dropdown linkage

--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,15 @@
-// get the select element
-const select = document.querySelector('#dropdown');
+const select = document.getElementById('dropdown');
+select.addEventListener('change', showSlider);
 
-// get all the div elements that should be toggled
-const europe = document.querySelector('.slider-Europe');
-const asia = document.querySelector('.slider-container');
-const northAmerica = document.querySelector('.slider-NorthAmerica');
-const southAmerica = document.querySelector('.slider-SouthAmerica');
-const oceania = document.querySelector('.slider-Oceania');
-const africa = document.querySelector('.slider-Africa');
-const antartica = document.querySelector('.slider-Antartica');
 
-// add an event listener to the select element
-select.addEventListener('change', function() {
-  // hide all the div elements
-  europe.style.display = 'none';
-  asia.style.display = 'none';
-  northAmerica.style.display = 'none';
-  southAmerica.style.display = 'none';
-  oceania.style.display = 'none';
-  africa.style.display = 'none';
-  antartica.style.display = 'none';
-
-  // show the selected div element based on the selected option
-  const selectedOption = select.options[select.selectedIndex].text;
-  switch (selectedOption) {
-    case 'Asia':
-      asia.style.display = 'block';
-      break;
-    case 'Europe':
-      europe.style.display = 'block';
-      break;
-    case 'North America':
-      northAmerica.style.display = 'block';
-      break;
-    case 'South America':
-      southAmerica.style.display = 'block';
-      break;
-    case 'Oceania':
-      oceania.style.display = 'block';
-      break;
-    case 'Africa':
-      africa.style.display = 'block';
-      break;
-    case 'Antartica':
-      antartica.style.display = 'block';
-      break;
+function showSlider() {
+    const sliderContainers = document.querySelectorAll('.slider-container');
+    sliderContainers.forEach(sliderContainer => {
+      if (sliderContainer.classList.contains(`slider-${select.value}`)) {
+        sliderContainer.style.display = 'block';
+      } else {
+        sliderContainer.style.display = 'none';
+      }
+    });
   }
-});
+  
