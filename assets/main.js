c5728b8f6047a566a770601457add362686fd76d
kinjalgawaskar <gawak052@newschool.edu>
2023-04-24 18:00:03 -0400

    JS function to show & hide divs on command

--- assets/main.js
+++ assets/main.js
@@ -7,9 +7,27 @@ function showSlider() {
     sliderContainers.forEach(sliderContainer => {
       if (sliderContainer.classList.contains(`slider-${select.value}`)) {
         sliderContainer.style.display = 'block';
+        console.log(sliderContainer.classList.contains(`slider-${select.value}`))
+
       } else {
         sliderContainer.style.display = 'none';
       }
     });
   }
   
+let searchValue = "New York"
+search()
+  function search() {
+    const sliderContainers = document.querySelectorAll('.slider-container');
+    sliderContainers.forEach(sliderContainer => {
+      let subtitle = sliderContainer.querySelector (".sub-title")
+      console.log (subtitle.innerHTML)
+      if (searchValue == subtitle.innerHTML) {
+        sliderContainer.style.display = 'block';
+      } else {
+        sliderContainer.style.display = 'none';
+      }
+    });
+  }
+  
+create an input button     whatever input has (console log it)     take the value and run serach function using that     
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,7 @@
-
-  // Making girl element droppable
-  const donkey = document.getElementById('kickass');
-  const girl = document.getElementById('kick');
-  
-  // Adding event listeners to the girl
-  girl.addEventListener('dragstart', dragStart);
-  
-  // Adding event listeners to the girl
-  donkey.addEventListener('dragover', dragOver);
-  donkey.addEventListener('drop', drop);
-  
-  // Function to handle the start of the dragging the girl
-  function dragStart(event) {
-    event.dataTransfer.setData('text', event.target.id);
-  }
-  
-  // Function to handle the girl dragover event
-  function dragOver(event) {
-    // Adding prevention of the default browser action (just for my personal reference)
-    event.preventDefault();
-  }
-  
-  // Adding function to handle the drop event
-  function drop(event) {
-    // Adding prevention of the default browser action (just for my personal reference)
-    event.preventDefault();
-  
-    // Fetching the girl ID from the data transfer
-    const id = event.dataTransfer.getData('text');
-  
-    // Get the girl
-    const element = document.getElementById(id);
-  
-    // Appending the girl to the drop target
-    event.target.appendChild(element);
-    window.open("customs.html", "_self");
-  
-  }
-  
+function moveLeft() {
+  var girl = document.getElementById("kick");
+  girl.classList.add("move");
+  setTimeout(function() {
+    window.location.href = "customs.html";
+  }, 500);
+}
