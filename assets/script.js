8ad2f4840a114a59b96117d8b74c4d7569606923
kinjalgawaskar <gawak052@newschool.edu>
2023-04-24 22:25:13 -0400

    shoe click event trial

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,10 @@ function playAudio() {
   var audio = document.getElementById("audio");
   audio.play();
 }
+
+function moveUp() {
+  var shoe = document.getElementById("shoe");
+  var currentPosition = parseInt(shoe.style.top) || 0;
+  var newPosition = currentPosition - 30;
+  shoe.style.top = newPosition + "px";
+}
